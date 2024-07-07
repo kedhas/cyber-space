8354fb0b057ff5dedc69bcb7865673fd3a7f97fb
Hassen Mohammed <kedhas@gmail.com>
2024-07-07 12:51:37 -0400

    init

---
+++ src/components/logo.js
@@ -0,0 +1,7 @@
+export default function Logo() {
+    return (
+        <div>
+            <span className="text-xl"><b><span className="text-purple-500">Purple</span>Transit</b></span>
+        </div>
+    )
+}
